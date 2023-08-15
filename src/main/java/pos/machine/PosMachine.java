d8f21d9b7f9d086336000166e167ddb0052ee0e7
Arianne <chiescake11@gmail.com>
2023-08-15 21:07:28 +0800

    feat: Added decodeToItems function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,23 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+
+    private List<ReceiptItem> decodeToItems(List<String> barcodes) {
+        List<Item> items = ItemsLoader.loadAllItems();
+        List<ReceiptItem> receiptItems = new ArrayList<>();
+
+        items.forEach(item -> {
+            int quantity = (int) barcodes.stream().filter(barcode -> barcodes.equals(item.getBarcode())).count();
+            ReceiptItem receiptItem = new ReceiptItem(item.getName(), quantity, item.getPrice(), 0);
+            receiptItems.add(receiptItem);
+        });
+
+        return receiptItems;
     }
 }
